95ba3a9458eeb4e79a24b2a8506d903bc161474c
inhyokim <inhyo.keem@gmail.com>
2025-07-23 09:39:55 +0900

    Add front0: Next.js project with home and about pages

---
+++ front0/src/app/about/page.js
@@ -0,0 +1,5 @@
+// app/about/page.jsx
+export default function AboutPage() {
+    return <h1>회사 소개</h1>;
+  }
+  
---
+++ front0/src/app/page.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+
+// app/page.jsx
+export default function Home() {
+  return <h1>홈페이지</h1>;
+}
